07421e0c992e917a941f4031128bb2285c405150
wozhendeshiyigedashuaibni <133622068+wozhendeshiyigedashuaibni@users.noreply.github.com>
2026-02-04 20:08:33 +0800

    Step 4b: Fix prisma import in auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import { prisma } from '../index.js';
+import { prisma } from '../prisma.js';
 
 const router = express.Router();
 
