105fefc5668ffe451faeeba5e7ec6286a650b37b
wozhendeshiyigedashuaibni <133622068+wozhendeshiyigedashuaibni@users.noreply.github.com>
2026-02-04 20:09:01 +0800

    Step 4e: Fix prisma import in leads.js

--- server/routes/leads.js
+++ server/routes/leads.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { prisma } from '../index.js';
+import { prisma } from '../prisma.js';
 import { upload } from '../middleware/upload.js';
 import { sendLeadEmail } from '../utils/email.js';
 
